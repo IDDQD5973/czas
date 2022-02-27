53d37bbc86bc06b635153b60e13a23dec3f542df
IDDQD5973 <93490240+IDDQD5973@users.noreply.github.com>
2022-02-27 16:19:04 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+    <input type="text"> </input>
+    <button>Dodaj</button>
+    <button>Cofnij</button>
+    </div>
+  );
+}
+
+export default App;
